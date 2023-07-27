45517dd47586e1e991954c01c954274e65b4afbc
Priya <priyamvadha0112@gmail.com>
2023-07-27 14:11:03 +0530

    changes in css

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -52,7 +52,7 @@ function Home() {
         </div>
       </div>
       <div className="skills">
-        <h1> Skills</h1>
+        <h1>SKILLS</h1>
         <ol className="list">
           <li className="item">
             <h2> Front-End</h2>
